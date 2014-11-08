c3bf94c7cbb7fcb05bf92288bb84fae0365700a0
Andrew Baerg <andrew@pullingshots.ca>
2014-11-08 00:23:45 -0700

    lots of improvements

--- Challenge2.pl
+++ Challenge2.pl
@@ -13,6 +13,12 @@ has 'gender' => (
 
 has 'health' => (
   is => 'rw',
+  default => sub { return 100 },
+);
+
+has 'score' => (
+  is => 'rw',
+  default => sub { return 0 },
 );
 
 has 'current_room' => (
@@ -22,19 +28,82 @@ has 'current_room' => (
 
 sub move {
   my ($self) = @_;
-  print "Which direction would you like to go? (n,s,e,w): ";
+
   my $direction;
+  my $question = Question->new(
+    options => {
+      North => 'n',
+      South => 's',
+      East => 'e',
+      West => 'w',
+      Up => 'u',
+      Down => 'd',
+    },
+    prompt => "Which direction would you like to go? (n)orth, (s)outh, (e)ast, (w)est, (u)p, or (d)own",
+    exception => "You can't go that way. (n)orth, (s)outh, (e)ast, (w)est, (u)p, or (d)own",
+  );
   while (!$direction) {
-    $direction = <>;
-    chomp $direction;
-    if ($self->current_room->can($direction) && $self->current_room->$direction) {
-    }
-    else {
+    $direction = $question->ask;
+    if (!$self->current_room->$direction) {
+      if ($direction eq 'u' || $direction eq 'd') {
+        say "there aren't any stairs or ladders here.";
+      }
+      else {
+        say "ouch! you ran into a wall!";
+      }
       $direction = undef;
-      print "You ran into a wall, try again (n,s,e,w): ";
     }
   }
+
   $self->current_room($self->current_room->$direction);
+}
+
+package Question;
+use Moose;
+
+has 'options' => (
+  is => 'rw',
+  isa => 'Maybe[HashRef]',
+  default => sub {
+    return {
+      Yes => 'y',
+      No => 'n',
+    }
+  }
+);
+
+has 'prompt' => (
+  is => 'rw',
+  default => 'Yes or No?',
+);
+
+has 'exception' => (
+  is => 'rw',
+  default => 'That is an invalid response, Yes or No?',
+);
+
+sub ask {
+  my ($self) = @_;
+
+  print $self->prompt . ": ";
+  my $answer;
+  while (!$answer) {
+    my $input = <>;
+    chomp $input;
+    if ($input && $self->options) {
+      exit if $input eq 'q';
+      foreach (keys %{$self->options}) {
+        if ($_ =~ /^$input/i) {
+          $answer = $self->options->{$_};
+        }
+      }
+    }
+    else {
+      $answer = $input;
+    }
+    print $self->exception . ": " if !$answer;
+  }
+  return $answer;
 }
 
 package Room;
@@ -80,6 +149,24 @@ has e => (
   },
 );
 
+has u => (
+  is => 'rw',
+  isa => 'Room',
+  trigger => sub {
+    my ( $self, $u, $old_u ) = @_;
+    $u->d($self) if !defined $old_u;
+  },
+);
+
+has d => (
+  is => 'rw',
+  isa => 'Room',
+  trigger => sub {
+    my ( $self, $d, $old_d ) = @_;
+    $d->u($self) if !defined $old_d;
+  },
+);
+
 has game => (
   is => 'ro',
   isa => 'CodeRef',
@@ -88,27 +175,36 @@ has game => (
 package Game;
 
 my $dungeon = Room->new(
-  description => "You are in a dark cell. There are doors on all 4 walls.",
+  description => "You find yourself in room. There are doors on all 4 walls.",
   n => Room->new(
-    description => "You are surrounded by cobwebs.",
-    game => sub { my ($player) = @_; say "Hello " . $player->name . "... you want to play a game?"; },
+    description => "The room you have entered is rather dark. You hear someone breathing in the corner.",
+    game => sub {
+      my ($player) = @_;
+      say "Hello " . $player->name . "... you want to play a game?";
+      
+    },
   ),
   s => Room->new(
-    description => "You see a stream of light coming through a barred window above you."
+    description => "The room you have entered is lit by a stream of light coming through a skylight in the ceiling.",
+  ),
+  e => Room->new(
+    description => "You have entered what appears to be a child's room.",
+  ),
+  w => Room->new(
+    description => "You are in the bathroom. Gotta pee?",
   ),
 );
 
-say "Welcome to my game!";
-print "What is your name? ";
-my $name = <>;
-chomp $name;
-print "What is your gender? ";
-my $gender = <>;
-chomp $gender;
+say "An adventure awaits!";
+my $name = Question->new( options => undef, prompt => "What is your name?", exception => "What is your name?" )->ask;
+my $gender = Question->new(
+  options => { male => 'male', female => 'female' },
+  prompt => "What is your gender? (m)ale or f(emale)",
+  exception => "What is your gender? (m)ale or f(emale)",
+)->ask;
 my $player = Player->new(
   name => $name,
   gender => $gender,
-  health => 100,
   current_room => $dungeon,
 );
 
